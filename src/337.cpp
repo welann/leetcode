42e7ccaa4cfd14657c318df1309c065001345861
welann <wzcing@outlook.com>
2023-09-18 17:10:55 +0800

    337 这题是抄的，开始时完全没思路，看了解答后才发现是这么轻松

---
+++ src/337.cpp
@@ -0,0 +1,60 @@
+#include <algorithm>
+#include <cmath>
+#include <cstddef>
+#include <functional>
+#include <iostream>
+#include <limits.h>
+#include <numeric>
+#include <queue>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    unordered_map <TreeNode*, int> f, g;
+
+    // void dfs(TreeNode* node) {
+    //     if (!node) {
+    //         return;
+    //     }
+    //     dfs(node->left);
+    //     dfs(node->right);
+    //     f[node] = node->val + g[node->left] + g[node->right];
+    //     g[node] = max(f[node->left], g[node->left]) + max(f[node->right], g[node->right]);
+    // }
+
+    int rob(TreeNode* root) {
+
+        function<void(TreeNode*)> dfs=[&](TreeNode* node){
+            if(!node){
+                return;
+            }
+            dfs(node->left);
+            dfs(node->right);
+            f[node]=node->val + g[node->left] + g[node->right];
+            g[node] = max(f[node->left], g[node->left]) + max(f[node->right], g[node->right]);
+        };
+
+        dfs(root);
+        return max(f[root], g[root]);
+    }
+
+};
+
+
+int main(){
+    return 0;
+}
